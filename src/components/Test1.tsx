fa876b70b42e7f15a1d77c6c1a9ec95ea05aefec
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2024-12-18 18:30:59 +0100

    test 2 done

--- src/components/Test1.tsx
+++ src/components/Test1.tsx
@@ -1,7 +1,7 @@
 export default function Test1() {
   return (
     <div className="min-h-screen bg-black flex items-center justify-center">
-      <h1 className="text-white text-9xl font-light">
+      <h1 className="text-white text-9xl font-title">
         DIGITAL
         <br />
         DESIGNER
