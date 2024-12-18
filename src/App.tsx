50e738d5ba4e4be8ce7a0dc50292dd6e0229ca1c
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2024-12-18 17:19:10 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Test1 from "./components/Test1"
+
+function App() {
+  return (
+    <>
+      <Test1></Test1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Test1.tsx
@@ -0,0 +1,9 @@
+const Test1 = () => {
+  return (
+    <div className="relative w-[94%] ml-[5rem]">
+      hello
+    </div>
+  )
+}
+
+export default Test1
