bce5fe7bc4e178bb7e37fb67efd3bff05c5fb8a9
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2024-12-27 15:15:41 +0100

    design4

--- src/components/Design4.tsx
+++ src/components/Design4.tsx
@@ -1,16 +1,36 @@
+import './design4.scss';
+import { useState } from 'react';  
+import { motion } from 'framer-motion';
+import useMousePosition from './utils/useMousePosition';
+
 export default function Design4() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen font-synco text-3xl bg-black text-white">
-            <div className="flex flex-col items-center justify-center h-screen">
-                <div className="flex items-center justify-center h-full">
-                    <p>This is the <span className="text-red-600">first paragraph</span> and if you move your mouse and <br/> hover over certain parts you will see a mask cursor effect.</p>
-                </div>
-            </div>
-            <div className="flex flex-col items-center justify-center h-screen">
-                <div className="flex items-center justify-center h-full">
-                <p>This is the <span className="text-red-600">second paragraph</span> and if you move your mouse and <br/> hover over certain parts you will see a mask cursor effect.</p>
-                </div>
-            </div>
-        </div>
-    );
-};
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const { x, y } = useMousePosition();
+  const size: number = isHovered ? 400 : 40;
+
+  return (
+    <main className='main'>
+      <motion.div 
+        className='mask'
+        animate={{
+          WebkitMaskPosition: `${(x ?? 0) - size / 2}px ${(y ?? 0) - size / 2}px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+      >
+        <p 
+          onMouseEnter={() => setIsHovered(true)} 
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          A visual designer - with skills that haven't been replaced by A.I (yet) - making good shit only if the paycheck is equally good.
+        </p>
+      </motion.div>
+
+      <div className='body'>
+        <p>
+          I'm a <span>selectively skilled</span> product designer with strong focus on producing high quality & impactful digital experience.
+        </p>
+      </div>
+    </main>
+  );
+}
