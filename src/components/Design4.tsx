6c8414e7e79038c0f5d1b4475e17903489f4b4c1
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2024-12-27 15:47:28 +0100

    design4 done

--- src/components/Design4.tsx
+++ src/components/Design4.tsx
@@ -1,36 +1,36 @@
 import './design4.scss';
-import { useState } from 'react';  
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import useMousePosition from './utils/useMousePosition';
 
 export default function Design4() {
-  const [isHovered, setIsHovered] = useState<boolean>(false);
-  const { x, y } = useMousePosition();
-  const size: number = isHovered ? 400 : 40;
+    const [isHovered, setIsHovered] = useState<boolean>(false);
+    const { x, y } = useMousePosition();
+    const size: number = isHovered ? 400 : 40;
 
-  return (
-    <main className='main'>
-      <motion.div 
-        className='mask'
-        animate={{
-          WebkitMaskPosition: `${(x ?? 0) - size / 2}px ${(y ?? 0) - size / 2}px`,
-          WebkitMaskSize: `${size}px`,
-        }}
-        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
-      >
-        <p 
-          onMouseEnter={() => setIsHovered(true)} 
-          onMouseLeave={() => setIsHovered(false)}
-        >
-        A mask effect is a visual technique used to hide or reveal parts of an element based on a defined shape or path, controlling its visibility.
-        </p>
-      </motion.div>
+    return (
+        <main className='main bg-black font-synco'>
+            <motion.div
+                className='mask'
+                animate={{
+                    WebkitMaskPosition: `${(x ?? 0) - size / 2}px ${(y ?? 0) - size / 2}px`,
+                    WebkitMaskSize: `${size}px`,
+                }}
+                transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+            >
+                <p
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
+                    A mask effect is a visual technique used to hide or reveal parts of an element based on a defined shape or path, controlling its visibility.
+                </p>
+            </motion.div>
 
-      <div className='body'>
-        <p className=''>
-        A mask effect ..<br /> <br /> <br /> <br /><span>.</span>
-        </p>
-      </div>
-    </main>
-  );
+            <div className='body'>
+                <p className=''>
+                    A mask effect . .<br /><br /><br /> <br /><span>.</span>
+                </p>
+            </div>
+        </main>
+    );
 }
