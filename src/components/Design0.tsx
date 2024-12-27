743a64097d2545c0c1a826f48b7d527a1616dfa2
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2024-12-27 16:27:00 +0100

    desing0 correction

--- src/components/Design0.tsx
+++ src/components/Design0.tsx
@@ -1,13 +1,13 @@
 export default function Design2() {
     return (
-        <div className="min-h-screen font-title">
+        <div className="min-h-screen min-w-screen font-title">
             <div className="bg-[#b4b440] p-5 h-screen flex flex-col justify-between">
                 <div className="flex justify-between">
                     <span>PEHSECHS</span>
                     <span>PAJAX</span>
                 </div>
 
-                <h1 className="text-9xl min-w-4xl h-fit text-center">
+                <h1 className="text-9xl min-w-4xl h-fit text-center font-bold">
                     <div>PA⑥</div>
                 </h1>
 
