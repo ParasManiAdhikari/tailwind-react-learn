b5bdb01ac72cdfe17427337c91bd9e04a698cda3
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2024-12-27 14:40:21 +0100

    design4 start

---
+++ src/components/Design4.tsx
@@ -0,0 +1,16 @@
+export default function Design4() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen font-synco text-3xl bg-black text-white">
+            <div className="flex flex-col items-center justify-center h-screen">
+                <div className="flex items-center justify-center h-full">
+                    <p>This is the <span className="text-red-600">first paragraph</span> and if you move your mouse and <br/> hover over certain parts you will see a mask cursor effect.</p>
+                </div>
+            </div>
+            <div className="flex flex-col items-center justify-center h-screen">
+                <div className="flex items-center justify-center h-full">
+                <p>This is the <span className="text-red-600">second paragraph</span> and if you move your mouse and <br/> hover over certain parts you will see a mask cursor effect.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
