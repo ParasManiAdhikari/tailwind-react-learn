2c40c3cf78c85ffd0f88864a88ffa3b154eeb5ad
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2024-12-28 12:47:11 +0100

    design5 done and dusted

--- src/components/Design5.tsx
+++ src/components/Design5.tsx
@@ -8,12 +8,9 @@ interface SplitTextProps {
 
 export default function Design5() {
 
-    // SplitText Component
-    function SplitText({ text = "Split Text Animation", className = "" }: SplitTextProps) {
-        // Split text into an array of characters
+    function SplitText({ text, className }: SplitTextProps) {
         const characters = text.split("")
 
-        // Animation configuration for each character
         const containerVariants = {
             hidden: { opacity: 0 },
             visible: (i = 1) => ({
@@ -85,11 +82,13 @@ export default function Design5() {
                 <h1 className="text-9xl text-center justify-center">
                     <div className="flex flex-col justify-center items-center">
                         {!submitted && (
-                            <SplitText text='. .' className="text-white text-8xl" />
+                            <div className="flex justify-center items-center space-x-2">
+                            <div className="w-24 h-24 bg-white rounded-full animate-bounce"></div>
+                          </div>
                         )}
 
                         {submitted && (
-                            <SplitText text={displayValue} className="text-white font-synco text-8xl" />
+                            <SplitText text={displayValue + "!"} className="text-white font-synco text-8xl" />
                         )}
 
                         <div className="flex flex-row text-4xl mt-20">
@@ -98,9 +97,9 @@ export default function Design5() {
                                 value={inputValue}
                                 onChange={handleInputChange}
                                 placeholder="Enter something"
-                                className="p-4 border-2 rounded-md"
+                                className="p-4 border-2 rounded-md bg-[#b4b440] text-white placeholder-yellow-200"
                             />
-                            <button onClick={handleSubmit} className="rounded-lg p-4 ml-2 bg-amber-200">
+                            <button onClick={handleSubmit} className="rounded-lg p-4 ml-2 bg-[#3e3e10] text-white">
                                 Play</button>
                         </div>
                     </div>
